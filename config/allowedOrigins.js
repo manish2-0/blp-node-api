b9289deb3b279df7ccd06374bdda33088147bb15
Manish <manishkumawat12955@gmail.com>
2023-03-08 16:33:00 +0530

    Commit deploy 9

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
+    'https://blp-node-api.vercel.app',
+    'https://blp-portal.vercel.app',
     'http://127.0.0.1:5500',
     'http://localhost:8000',
     'http://localhost:3000'
